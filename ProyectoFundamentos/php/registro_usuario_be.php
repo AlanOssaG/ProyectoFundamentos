4b31952c7b6137993313c95cf9fa91208c86e525
iris1404 <147352871+iris1404@users.noreply.github.com>
2023-10-14 23:41:15 -0500

    Update registro_usuario_be.php

--- ProyectoFundamentos/php/registro_usuario_be.php
+++ ProyectoFundamentos/php/registro_usuario_be.php
@@ -10,23 +10,61 @@
    $query = "INSERT INTO usuarios(nombre_completo,  correo, usuario, contrasena) 
              VALUES('$nombre_completo', '$correo', '$usuario', '$contrasena')";
     
+
+ //Verificar que el correo no se repita en caso de existir en la base de datos
+  
+ $verificar_correo = mysqli_query($conexion, "SELECT * FROM  usuarios WHERE correo = '$correo' ");
+
+ if(mysqli_num_rows($verificar_correo)>0){
+
+     echo ' 
+     <script>
+             alert("Este correo ya se encuentra registrado, verifica que lo hayas ingresado correctamente o intenta con otro diferente");
+             window.location = "../index.html";
+     </script>
+ 
+     ';
+    exit();
+ 
+}   
+
+//Verificar que el nombre del usuario no se repite en caso de existir en la base de datos
+
+$verificar_usuario = mysqli_query($conexion, "SELECT * FROM  usuarios WHERE usuario = '$usuario' ");
+
+if(mysqli_num_rows($verificar_usuario) > 0){
+
+    echo ' 
+    <script>
+            alert("Este usuario ya se encuentra registrado, intenta con otro diferente");
+            window.location = "../index.html";
+    </script>
+
+    ';
+    exit();
+}   
+
+
+      
+
     $ejecutar = mysqli_query($conexion, $query);
 
     if($ejecutar){
         echo '
            <script>
-               alert("Usuario almacenado exitosamente")
-               window.location = "../index.html"
+               alert("Usuario almacenado exitosamente");
+               window.location = "../index.html";
            </script>
         ';
     }else{
         echo '
         <script>
-            alert("Error, Intentalo de nuevo")
-            window.location = "../index.html"
+            alert("Error, Intentalo de nuevo");
+            window.location = "../index.html";
         </script>
      ';
     }
 
     mysqli_close($conexion);   
 ?>
+
